c501383b940a0dded6676910b66ddb5aa12e5009
LazyWood <462076249@qq.com>
2023-04-19 15:15:07 +0800

    好的，关于“EnemyController”类，我们已经创建完毕了。 现在我需要一个用于控制敌机的生成的类，它应该负责敌机的创建，以及敌机的销毁。 敌机应该被创建在屏幕顶部之上，创建的位置是随机的，创建的时间间隔也是随机的。 敌机应该被销毁，当敌机的位置超出了屏幕底部的位置。
    可以不要用触发器来判断是否离开屏幕范围，而是使用屏幕位置来计算出什么时候离开屏幕吗？
    
    请在“EnemySpawner ”的基础上修改离开屏幕的判断逻辑

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 3f;
+
+    private float screenWidth;
+    private float screenHeight;
+
+    private void Start()
+    {
+        // 获取屏幕尺寸
+        Camera mainCamera = Camera.main;
+        screenHeight = mainCamera.orthographicSize;
+        screenWidth = mainCamera.aspect * screenHeight;
+
+        // 开始生成敌机
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            // 等待随机的时间
+            float randomDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+            yield return new WaitForSeconds(randomDelay);
+
+            // 随机生成敌机的位置
+            float randomX = Random.Range(-screenWidth, screenWidth);
+            Vector3 spawnPosition = new Vector3(randomX, screenHeight, 0f);
+
+            // 创建敌机
+            GameObject newEnemy = Instantiate(enemyPrefab, spawnPosition, enemyPrefab.transform.rotation);
+
+            // 销毁超出屏幕范围的敌机
+            StartCoroutine(DestroyIfOffScreen(newEnemy));
+        }
+    }
+
+    private IEnumerator DestroyIfOffScreen(GameObject enemy)
+    {
+        while (true)
+        {
+            // 如果敌机位置超出屏幕范围，则销毁敌机
+            Vector3 screenPoint = Camera.main.WorldToViewportPoint(enemy.transform.position);
+            if (screenPoint.y < 0)
+            {
+                Destroy(enemy);
+                break;
+            }
+
+            yield return null;
+        }
+    }
+}
