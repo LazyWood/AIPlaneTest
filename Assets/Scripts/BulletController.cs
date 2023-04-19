f315f3b2a11edf92c3f326f9459b86f32d0d254b
LazyWood <462076249@qq.com>
2023-04-19 13:59:09 +0800

    现在我们有了玩家控制类“PlayerController ”和子弹控制类“BulletController ”。接下来我希望在玩家控制类“PlayerController ”中加入发射子弹的功能，玩家可以点击空格键或点击鼠标按钮来发射子弹。

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed; // 子弹的速度
+    public float lifeTime; // 子弹的生命周期
+
+    private void Start()
+    {
+        // 在lifeTime秒后销毁子弹
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        // 让子弹沿着y轴正方向移动
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 如果子弹接触到敌机，则销毁子弹和敌机
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f; // 玩家战机的移动速度
-    private Vector2 screenBounds; // 屏幕边界
-    private float objectWidth; // 玩家战机的宽度
-    private float objectHeight; // 玩家战机的高度
-
-    private void Start()
-    {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2f;
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2f;
-    }
+    public float speed; // 玩家移动速度
+    public GameObject bulletPrefab; // 子弹预制体
+    public Transform bulletSpawnPoint; // 子弹生成点
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // 获取水平输入
-        float verticalInput = Input.GetAxis("Vertical"); // 获取垂直输入
+        // 处理玩家移动逻辑
+        Move();
 
-        // 计算移动距离
-        float horizontalMovement = horizontalInput * speed * Time.deltaTime;
-        float verticalMovement = verticalInput * speed * Time.deltaTime;
+        // 处理玩家发射子弹逻辑
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
 
-        // 移动玩家战机
-        transform.Translate(horizontalMovement, verticalMovement, 0);
+    private void Move()
+    {
+        // 处理玩家移动逻辑
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        // 限制玩家战机在屏幕内移动
-        Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
-        transform.position = viewPos;
+        Vector2 direction = new Vector2(h, v);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // 确保玩家不会移出屏幕
+        float x = Mathf.Clamp(transform.position.x, -8.9f, 8.9f);
+        float y = Mathf.Clamp(transform.position.y, -3.7f, 3.7f);
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+    private void Shoot()
+    {
+        // 生成子弹
+        Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
     }
 }
