0c67b0976be18b2b1306ea4358503594974ee610
LazyWood <462076249@qq.com>
2023-04-19 14:48:54 +0800

    首先我会将敌机对象旋转180度，然后请你为我创建敌机对应的脚本，敌机从屏幕顶部向屏幕底部以随机的速度移动，当敌机位置完全超过屏幕底部的时候，销毁敌机。敌机会以随机的速度发射子弹。

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,6 +6,7 @@ public class BulletController : MonoBehaviour
 {
     public float speed; // 子弹的速度
     public float lifeTime; // 子弹的生命周期
+    public Vector2 direction = Vector2.up;
 
     private void Start()
     {
@@ -16,7 +17,7 @@ public class BulletController : MonoBehaviour
     private void Update()
     {
         // 让子弹沿着y轴正方向移动
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        transform.Translate(direction *Vector2.up * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float minSpeed = 1f;
+    public float maxSpeed = 5f;
+    public float bulletSpeed = 5f;
+    public float bulletCooldown = 1f;
+
+    private float speed;
+    private float bulletTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set random speed for the enemy
+        speed = Random.Range(minSpeed, maxSpeed);
+        // Set random bullet timer for the enemy
+        bulletTimer = Random.Range(0f, bulletCooldown);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the enemy downwards
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        // Check if it's time to shoot a bullet
+        bulletTimer -= Time.deltaTime;
+        if (bulletTimer <= 0)
+        {
+            // Reset bullet timer
+            bulletTimer = bulletCooldown;
+
+            // Shoot a bullet
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            BulletController bulletController = bullet.GetComponent<BulletController>();
+            if (bulletController != null)
+            {
+                bulletController.speed = bulletSpeed;
+                bulletController.direction = Vector2.down;
+            }
+        }
+    }
+}
